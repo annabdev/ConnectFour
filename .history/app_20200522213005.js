5bbc2d03b66cfcb04155da950f8f01f95ad41ddc
annabdev <beagley.dev@gmail.com>
2020-05-22 21:37:43 -0600

    first commit

---
+++ .history/app_20200522213005.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelectorAll('.grid div')
+    const result = document.querySelector('#result')
+    const displayCurrentPlayer = document.querySelector('#current-player')
+    let currentPlayer = 1
+
+    for (var i = 0, len = squares.length; i < len; i++)
+
+    (function(index){
+
+        squares[i].onclick = function() {
+            if(squares[index + 7].classList.contains('taken')) {
+                if 
+            }
+        }
+    })
+})
